cd8e76e445aa57341f8fddac83a03f42b268201a
romovababka <sofiiagutgarts@gmail.com>
2022-07-12 15:13:12 +0100

    remove html/change it to js
    remove all html, now all information about menu shows on page via js(map and take information from fooditems.js)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log(food);
 
 function displayItems() {
     let kfc = document.getElementById("kfc");
-    let mc = document.getElementById("mc");
+    let donner = document.getElementById("donner");
     let ice_cream = document.getElementById("ice-cream");
     let chinese = document.getElementById("Chinese");
 
@@ -53,6 +53,120 @@ function displayItems() {
         itemCard.appendChild(itemPrice);
 
         kfc.appendChild(itemCard);
+    })
+
+    donnerData.map(item=> {
+        let itemCard = document.createElement("div");
+        itemCard.setAttribute("id", "item-card");
+
+        let cardTop = document.createElement("div");
+        cardTop.setAttribute("id", "card-top")
+
+        let star = document.createElement("i");
+        star.setAttribute("class", "fa fa-star");
+        star.setAttribute("id", "rating");
+        star.innerHTML = " " + item.kcal;
+
+        let heart = document.createElement("i");
+        heart.setAttribute("class", "fa fa-heart-o add-to-cart");
+        heart.setAttribute("id", item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        let img = document.createElement("img");
+        img.src = item.img;
+
+        let itemName = document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText = item.name;
+
+        let itemPrice = document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText = item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        donner.appendChild(itemCard);
+    })
+
+    chineseData.map(item=> {
+        let itemCard = document.createElement("div");
+        itemCard.setAttribute("id", "item-card");
+
+        let cardTop = document.createElement("div");
+        cardTop.setAttribute("id", "card-top")
+
+        let star = document.createElement("i");
+        star.setAttribute("class", "fa fa-star");
+        star.setAttribute("id", "rating");
+        star.innerHTML = " " + item.kcal;
+
+        let heart = document.createElement("i");
+        heart.setAttribute("class", "fa fa-heart-o add-to-cart");
+        heart.setAttribute("id", item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        let img = document.createElement("img");
+        img.src = item.img;
+
+        let itemName = document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText = item.name;
+
+        let itemPrice = document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText = item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        chinese.appendChild(itemCard);
+    })
+
+    iceData.map(item=> {
+        let itemCard = document.createElement("div");
+        itemCard.setAttribute("id", "item-card");
+
+        let cardTop = document.createElement("div");
+        cardTop.setAttribute("id", "card-top")
+
+        let star = document.createElement("i");
+        star.setAttribute("class", "fa fa-star");
+        star.setAttribute("id", "rating");
+        star.innerHTML = " " + item.kcal;
+
+        let heart = document.createElement("i");
+        heart.setAttribute("class", "fa fa-heart-o add-to-cart");
+        heart.setAttribute("id", item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        let img = document.createElement("img");
+        img.src = item.img;
+
+        let itemName = document.createElement("p");
+        itemName.setAttribute("id", "item-name");
+        itemName.innerText = item.name;
+
+        let itemPrice = document.createElement("p");
+        itemPrice.setAttribute("id", "item-price");
+        itemPrice.innerText = item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        ice_cream.appendChild(itemCard);
     })
 }
 displayItems();
